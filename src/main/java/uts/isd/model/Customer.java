2f81cd8b3650198332e1192968e63a9d2bdf246a
iMystic <clarence.e.cheung@gmail.com>
2021-05-01 12:01:27 +1000

    added customer model

---
+++ src/main/java/uts/isd/model/Customer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.model;
+import java.io.*;
+/**
+ *
+ * @author Clare
+ */
+public class Customer extends User implements Serializable {
+    private String customerId;
+
+    public Customer(String name, String email, String password, String gender, String dob, String phone, String address) {
+        super(name, email, password, gender, dob, phone, address);
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+    
+    
+}
