6f2b1c0bdbacad982988bfde03bd6f30de2c9b38
hunter-blaze <50227741+hunter-blaze@users.noreply.github.com>
2021-05-08 12:39:37 +1000

    add product servlet, validator and product

--- src/main/java/uts/isd/controller/Validator.java
+++ src/main/java/uts/isd/controller/Validator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uts.isd.controller;
 
 import java.io.Serializable;
@@ -10,44 +5,77 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author Charl
- */
-public class Validator implements Serializable{
+public class Validator implements Serializable {
+
     private String emailPattern = "([a-zA-Z0-9]+)(([._-])([a-zA-Z0-9]+))*(@)([a-z]+)(.)([a-z]{3})((([.])[a-z]{0,2})*)";
     private String namePattern = "([A-Z][a-z]+[\\s])+[A-Z][a-z]*";
     private String passwordPattern = "[a-z0-9]{4,}";
-    
-    public Validator() {  }
-    
-    public boolean validate(String pattern, String input){
+
+    // Product Patterns
+    private String prod_NamePattern = "[^\'\"]+";
+    private String prod_PricePattern = "^[0-9]{1,10}(.[0-9]{1,2})?$";
+    private String prod_DescPattern = "[^\'\"]+";
+    private String prod_TypePattern = "^[a-zA-Z\\s]+$";
+    private String prod_quantPattern = "^[0-9]{1,10}$";
+
+    public Validator() {
+    }
+
+    public boolean validate(String pattern, String input) {
         Pattern regEx = Pattern.compile(pattern);
         Matcher match = regEx.matcher(input);
-        
+
         return match.matches();
     }
-    
-    public boolean checkEmpty(String email, String password){
+
+    public boolean checkEmpty(String email, String password) {
         return email.isEmpty() || password.isEmpty();
     }
-    
-    public boolean validateEmail(String email){
-        return validate(emailPattern,email);
+
+    public boolean validateEmail(String email) {
+        return validate(emailPattern, email);
     }
-    
-    public boolean validateName(String name){
+
+    public boolean validateName(String name) {
         return validate(namePattern, name);
     }
-    
-    public boolean validatePassword(String password){
+
+    public boolean validatePassword(String password) {
         return validate(passwordPattern, password);
     }
+
+    // Validate Product
+    public boolean validateProductName(String name) {
+        return validate(prod_NamePattern, name);
+    }
     
-    public void clear(HttpSession session){
-        session.setAttribute("emailErr","Enter email");
-        session.setAttribute("passErr","Enter password");
-        session.setAttribute("exitErr","");
-        session.setAttribute("nameErr","Enter name");
+    public boolean validateProductPrice(String price) {
+        return validate(prod_PricePattern, price);
+    }
+    
+    public boolean validateProductDesc(String detail) {
+        return validate(prod_DescPattern, detail);
+    }
+
+    public boolean validateProductType(String type) {
+        return validate(prod_TypePattern, type);
+    }
+
+    public boolean validateQuantity(String quantity) {
+        return validate(prod_quantPattern, quantity);
+    }
+
+    public void clear(HttpSession session) {
+        session.setAttribute("emailErr", "Enter email");
+        session.setAttribute("passErr", "Enter password");
+        session.setAttribute("exitErr", "");
+        session.setAttribute("nameErr", "Enter name");
+        
+        // Product session
+        session.setAttribute("productNameErr", "Enter Product Name");
+        session.setAttribute("productPriceErr", null);
+        session.setAttribute("productdescErr", "Enter Description");
+        session.setAttribute("productTypeErr", "Enter Type");
+        session.setAttribute("productQuantityErr", null);
     }
 }
