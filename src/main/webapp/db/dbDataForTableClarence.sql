43f606030c47ad342aea4a5fa91d64209ec40f1c
iMystic <clarence.e.cheung@gmail.com>
2021-05-12 16:01:44 +1000

    gay

--- src/main/webapp/db/dbDataForTableClarence.sql
+++ src/main/webapp/db/dbDataForTableClarence.sql
@@ -1,50 +1,5 @@
--- The Invoice_ID and Order_ID need to completed before inserting these values.
--- INSERT INTO SHIPMENT(SHIPMENT_TRACKING_NO,SHIPMENT_DATE,SHIPMENT_DETAILS)
--- VALUES
--- ('JFRBQP7QIMLH','01/01/2021 11:04:27','No of Items: 1 | Total Weight: 200g  | Box Size: Small'),
--- ('4FKX5HQBQG2P','08/04/2021 04:31:47','No of Items: 5 | Total Weight: 1053g | Box Size: Large'),
--- ('0OHNK76OWR7R','07/03/2021 17:50:32','No of Items: 1 | Total Weight: 358g  | Box Size: Small'),
--- ('G7EGCD2OM5AP','14/02/2021 22:55:59','No of Items: 1 | Total Weight: 892g  | Box Size: Medium'),
--- ('GAUXOQMY9C7R','03/04/2021 02:04:04','No of Items: 2 | Total Weight: 658g  | Box Size: Small'),
--- ('F71KBIMV231K','03/04/2021 07:13:46','No of Items: 1 | Total Weight: 423g  | Box Size: Small'),
--- ('ZRHD8NX5S1BV','19/01/2021 11:02:46','No of Items: 1 | Total Weight: 991g  | Box Size: Large'),
--- ('H7CFI5BSUTA9','22/04/2021 14:24:37','No of Items: 3 | Total Weight: 450g  | Box Size: Medium'),
--- ('XCGAOH5M0GJ6','22/04/2021 06:30:07','No of Items: 1 | Total Weight: 87g   | Box Size: Medium'),
--- ('VO27LWVDE1PK','10/01/2021 23:51:18','No of Items: 2 | Total Weight: 2330g | Box Size: Small'),
--- ('JWD5UMIHC4US','08/03/2021 22:01:11','No of Items: 4 | Total Weight: 2000g | Box Size: Large'),
--- ('AEKF7FRMHJCR','18/01/2021 09:45:41','No of Items: 1 | Total Weight: 434g  | Box Size: Small'),
--- ('L5U2CEOW937I','09/04/2021 18:41:26','No of Items: 1 | Total Weight: 876g  | Box Size: Large'),
--- ('5TBFOKHDEDB5','11/04/2021 11:42:42','No of Items: 2 | Total Weight: 2132g | Box Size: Medium'),
--- ('4VUEPHTB4X70','06/01/2021 21:04:51','No of Items: 1 | Total Weight: 765g  | Box Size: Small'),
--- ('R071PKA7N5RQ','11/03/2021 05:36:06','No of Items: 1 | Total Weight: 2432g | Box Size: Medium'),
--- ('129VV3IDKJWR','04/03/2021 20:42:26','No of Items: 1 | Total Weight: 6898g | Box Size: Large'),
--- ('EK5CDCTHC38D','11/01/2021 02:50:11','No of Items: 1 | Total Weight: 634g  | Box Size: Small'),
--- ('RTLU5BM9AMWK','06/04/2021 19:17:32','No of Items: 2 | Total Weight: 953g  | Box Size: Large'),
--- ('53OIERPAA23P','22/02/2021 13:15:20','No of Items: 3 | Total Weight: 905g  | Box Size: Medium');
 
--- These names could be used for the CUSTOMER or STAFF
--- INSERT INTO PAYMENT(CREDIT_CARD_NO,EXPIRY_DATE,SECURITY_NO,OWNER_NAME)
--- VALUES
--- ('4929173930006460','08/21','313','Gilberto Boyd'),
--- ('4539739839449860','02/23','730','Mabel Osborne'),
--- ('5292168973269617','06/22','894','Frankie Wong'),
--- ('6011869167040429','07/22','828','Jenny Barton'),
--- ('373543407522463','08/22','232','Bert Cross'),
--- ('372670995228475','11/23','913','Jean Meyer'),
--- ('6011685489117996','01/24','039','Amos Bryan'),
--- ('5148885120269846','01/24','734','Archie Adkins'),
--- ('4556171645221465','04/23','705','Craig Austin'),
--- ('4539762109128519','08/24','489','Michael Steele'),
--- ('4024007138918270','10/25','996','Pamela Ford'),
--- ('6011296043284676','02/23','958','Beulah Curry'),
--- ('5499926173256135','09/22','966','Percy Chambers'),
--- ('5199004694447600','07/22','847','Daisy Baker'),
--- ('5460429054649267','05/21','416','Kara Gibson'),
--- ('6011551047373317','03/23','515','Micheal Colon'),
--- ('342275735127842','04/24','810','Nichole Perez'),
--- ('6011970342978788','05/22','865','Lela Jensen'),
--- ('343247561690929','03/22','284','Nadine Moran'),
--- ('349714273935797','07/23','602','Sheri Fletcher');
+
 
 -- this would be the SQL statement that would insert data into the User table 
 INSERT INTO "USER"(EMAIL, "NAME", PASSWORD, GENDER, PHONE, DOB, ADDRESS, "ROLE", ACTIVATED)
@@ -74,7 +29,11 @@ VALUES
 
 INSERT INTO "STAFF"(ID, "POSITION")
 VALUES
-(10000, 'Pedo');
+(10000, 'Manager');
+
+INSERT INTO CUSTOMER(ID, "TYPE")
+VALUES
+(10012, 'Individual');
 
 
 -- INSERT 20 instances into PRODUCT table
@@ -197,3 +156,51 @@ VALUES
 (103,117,'07/05/2021',''),
 (112,118,'09/05/2021',''),
 (101,119,'12/05/2021','');
+
+-- These names could be used for the CUSTOMER or STAFF
+INSERT INTO PAYMENT(INVOICE_ID, CREDIT_CARD_NO,EXPIRY_DATE,SECURITY_NO,OWNER_NAME)
+VALUES
+(100, '4929173930006460','08/21','313','Gilberto Boyd'),
+(100, '4539739839449860','02/23','730','Mabel Osborne'),
+(100, '5292168973269617','06/22','894','Frankie Wong'),
+(100, '6011869167040429','07/22','828','Jenny Barton'),
+(100, '373543407522463','08/22','232','Bert Cross'),
+(100, '372670995228475','11/23','913','Jean Meyer'),
+(100, '6011685489117996','01/24','039','Amos Bryan'),
+(100, '5148885120269846','01/24','734','Archie Adkins'),
+(100, '4556171645221465','04/23','705','Craig Austin'),
+(100, '4539762109128519','08/24','489','Michael Steele'),
+(100, '4024007138918270','10/25','996','Pamela Ford'),
+(100, '6011296043284676','02/23','958','Beulah Curry'),
+(100, '5499926173256135','09/22','966','Percy Chambers'),
+(100, '5199004694447600','07/22','847','Daisy Baker'),
+(100, '5460429054649267','05/21','416','Kara Gibson'),
+(100, '6011551047373317','03/23','515','Micheal Colon'),
+(100, '342275735127842','04/24','810','Nichole Perez'),
+(100, '6011970342978788','05/22','865','Lela Jensen'),
+(100, '343247561690929','03/22','284','Nadine Moran'),
+(100, '349714273935797','07/23','602','Sheri Fletcher');
+
+-- The Invoice_ID and Order_ID need to completed before inserting these values.
+INSERT INTO SHIPMENT(INVOICE_ID, ORDER_ID, SHIPMENT_TRACKING_NO,SHIPMENT_DATE,SHIPMENT_DETAILS)
+VALUES
+(100, 100, 'JFRBQP7QIMLH','01/01/2021 11:04:27','No of Items: 1 | Total Weight: 200g  | Box Size: Small'),
+(100, 100, '4FKX5HQBQG2P','08/04/2021 04:31:47','No of Items: 5 | Total Weight: 1053g | Box Size: Large'),
+(100, 100, '0OHNK76OWR7R','07/03/2021 17:50:32','No of Items: 1 | Total Weight: 358g  | Box Size: Small'),
+(100, 100, 'G7EGCD2OM5AP','14/02/2021 22:55:59','No of Items: 1 | Total Weight: 892g  | Box Size: Medium'),
+(100, 100, 'GAUXOQMY9C7R','03/04/2021 02:04:04','No of Items: 2 | Total Weight: 658g  | Box Size: Small'),
+(100, 100, 'F71KBIMV231K','03/04/2021 07:13:46','No of Items: 1 | Total Weight: 423g  | Box Size: Small'),
+(100, 100, 'ZRHD8NX5S1BV','19/01/2021 11:02:46','No of Items: 1 | Total Weight: 991g  | Box Size: Large'),
+(100, 100, 'H7CFI5BSUTA9','22/04/2021 14:24:37','No of Items: 3 | Total Weight: 450g  | Box Size: Medium'),
+(100, 100, 'XCGAOH5M0GJ6','22/04/2021 06:30:07','No of Items: 1 | Total Weight: 87g   | Box Size: Medium'),
+(100, 100, 'VO27LWVDE1PK','10/01/2021 23:51:18','No of Items: 2 | Total Weight: 2330g | Box Size: Small'),
+(100, 100, 'JWD5UMIHC4US','08/03/2021 22:01:11','No of Items: 4 | Total Weight: 2000g | Box Size: Large'),
+(100, 100, 'AEKF7FRMHJCR','18/01/2021 09:45:41','No of Items: 1 | Total Weight: 434g  | Box Size: Small'),
+(100, 100, 'L5U2CEOW937I','09/04/2021 18:41:26','No of Items: 1 | Total Weight: 876g  | Box Size: Large'),
+(100, 100, '5TBFOKHDEDB5','11/04/2021 11:42:42','No of Items: 2 | Total Weight: 2132g | Box Size: Medium'),
+(100, 100, '4VUEPHTB4X70','06/01/2021 21:04:51','No of Items: 1 | Total Weight: 765g  | Box Size: Small'),
+(100, 100, 'R071PKA7N5RQ','11/03/2021 05:36:06','No of Items: 1 | Total Weight: 2432g | Box Size: Medium'),
+(100, 100, '129VV3IDKJWR','04/03/2021 20:42:26','No of Items: 1 | Total Weight: 6898g | Box Size: Large'),
+(100, 100, 'EK5CDCTHC38D','11/01/2021 02:50:11','No of Items: 1 | Total Weight: 634g  | Box Size: Small'),
+(100, 100, 'RTLU5BM9AMWK','06/04/2021 19:17:32','No of Items: 2 | Total Weight: 953g  | Box Size: Large'),
+(100, 100, '53OIERPAA23P','22/02/2021 13:15:20','No of Items: 3 | Total Weight: 905g  | Box Size: Medium');
